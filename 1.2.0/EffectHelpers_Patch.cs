88b68410be625859244c5519f6924e8e73cf59bd
Zoey D. <zdavis8529@gmail.com>
2024-08-11 01:42:06 -0600

    Fix Effects

--- 1.2.0/EffectHelpers_Patch.cs
+++ 1.2.0/EffectHelpers_Patch.cs
@@ -10,6 +10,7 @@ namespace KitchenPlatePatch._120
     [HarmonyPatch(typeof(EffectHelpers))]
     static class EffectHelpers_Patch
     {
+        [HarmonyPatch(nameof(EffectHelpers.AddAttachedEffectComponents))]
         [HarmonyPrefix]
         [HarmonyPriority(int.MinValue)]
         static bool AddAttachedEffectComponents_Prefix(bool __runOriginal, EntityCommandBuffer ecb, Entity e, Effect eff)
@@ -23,6 +24,25 @@ namespace KitchenPlatePatch._120
             }
 
             return false;
+        }
+
+        [HarmonyPatch(nameof(EffectHelpers.AddApplianceEffectComponents))]
+        [HarmonyPrefix]
+        [HarmonyPriority(int.MinValue)]
+        static bool AddApplianceEffectComponents_Prefix(bool __runOriginal, EntityCommandBuffer ecb, Entity e, IEffectPropertySource prop)
+        {
+            if (!__runOriginal || prop.EffectType == null || prop.EffectRange == null)
+                return true;
+
+            if (prop.EffectCondition != null)
+                ecb.AddComponent(e, (dynamic)prop.EffectCondition);
+            else 
+                ecb.AddComponent<CEffectAlways>(e);
+
+            ecb.AddComponent(e, (dynamic)prop.EffectType);
+            ecb.AddComponent(e, (dynamic)prop.EffectRange);
+
+            return true;
         }
     }
 }
