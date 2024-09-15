53e1013e289fe7cbea5a705ff074fed8ad487c4e
Zoey D. <zdavis8529@gmail.com>
2024-09-15 09:55:06 -0600

    added depthstep & removed KL dependency

--- 1.2.0/IViewDataFormatterPatches.cs
+++ 1.2.0/IViewDataFormatterPatches.cs
@@ -1,7 +1,6 @@
 ﻿using HarmonyLib;
 using Kitchen;
 using Kitchen.Formatters.Kitchen;
-using KitchenLib.Utils;
 using MessagePack;
 using MessagePack.Resolvers;
 using System;
@@ -19,12 +18,12 @@ namespace KitchenPlatePatch._120
     [HarmonyPatch(typeof(IViewDataFormatter))]
     static class IViewDataFormatter_Patch
     {
-        private static FieldInfo _typeToKeyAndJumpMap = ReflectionUtils.GetField<IViewDataFormatter>("typeToKeyAndJumpMap");
+        private static readonly FieldInfo _typeToKeyAndJumpMap = typeof(IViewDataFormatter).GetField("typeToKeyAndJumpMap");
 
         // Serialize
         [HarmonyPatch(nameof(IViewDataFormatter.Serialize)), HarmonyPrefix]
         [HarmonyPriority(Priority.LowerThanNormal)]
-        static bool Serialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref MessagePackWriter writer, IViewData value, MessagePackSerializerOptions options)
+        static bool Serialize_Prefix(bool __runOriginal, ref IViewDataFormatter __instance, ref MessagePackWriter writer, ref IViewData value, MessagePackSerializerOptions options)
         {
             if (!__runOriginal || _typeToKeyAndJumpMap.IsNullOrDestroyed() || value == null)
                 return true;
@@ -33,6 +32,7 @@ namespace KitchenPlatePatch._120
             if (typeToKeyAndJumpMap.ContainsKey(value.GetType().TypeHandle))
                 return true;
 
+            writer.WriteArrayHeader(2);
             writer.WriteInt32(784);
             StandardResolver.Instance.GetFormatterWithVerify<IViewData>().Serialize(ref writer, value, options);
             return false;
@@ -41,16 +41,19 @@ namespace KitchenPlatePatch._120
         // Deserialize
         [HarmonyPatch(nameof(IViewDataFormatter.Deserialize)), HarmonyPrefix]
         [HarmonyPriority(Priority.LowerThanNormal)]
-        static bool Deserialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref IViewData __result, MessagePackReader reader, MessagePackSerializerOptions options)
+        static bool Deserialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref IViewData __result, ref MessagePackReader reader, MessagePackSerializerOptions options)
         {
             if (!__runOriginal || reader.TryReadNil() || reader.ReadArrayHeader() != 2 || _typeToKeyAndJumpMap.IsNullOrDestroyed())
                 return true;
 
-            int modDetector = reader.ReadInt32();
+            var pseudoReader = reader.Clone(reader.Sequence);
+            options.Security.DepthStep(ref pseudoReader); // don't want to DepthStep if the reader will have such repeated if it is unmodded
+            int modDetector = pseudoReader.ReadInt32();
             if (modDetector != 784)
                 return true;
 
-            __result = StandardResolver.Instance.GetFormatterWithVerify<IViewData>().Deserialize(ref reader, options);
+            options.Security.DepthStep(ref reader);
+            __result = StandardResolver.Instance.GetFormatterWithVerify<IViewData>().Deserialize(ref pseudoReader, options);
             return false;
         }
     }
