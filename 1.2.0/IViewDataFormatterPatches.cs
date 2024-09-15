bf8a871e9f7563e8abb0f707fbe7e62dd00bbad7
Zoey D. <zdavis8529@gmail.com>
2024-09-15 10:01:38 -0600

    undo

--- 1.2.0/IViewDataFormatterPatches.cs
+++ 1.2.0/IViewDataFormatterPatches.cs
@@ -41,19 +41,16 @@ namespace KitchenPlatePatch._120
         // Deserialize
         [HarmonyPatch(nameof(IViewDataFormatter.Deserialize)), HarmonyPrefix]
         [HarmonyPriority(Priority.LowerThanNormal)]
-        static bool Deserialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref IViewData __result, ref MessagePackReader reader, MessagePackSerializerOptions options)
+        static bool Deserialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref IViewData __result, MessagePackReader reader, MessagePackSerializerOptions options)
         {
             if (!__runOriginal || reader.TryReadNil() || reader.ReadArrayHeader() != 2 || _typeToKeyAndJumpMap.IsNullOrDestroyed())
                 return true;
 
-            var pseudoReader = reader.Clone(reader.Sequence);
-            options.Security.DepthStep(ref pseudoReader); // don't want to DepthStep if the reader will have such repeated if it is unmodded
-            int modDetector = pseudoReader.ReadInt32();
+            int modDetector = reader.ReadInt32();
             if (modDetector != 784)
                 return true;
 
-            options.Security.DepthStep(ref reader);
-            __result = StandardResolver.Instance.GetFormatterWithVerify<IViewData>().Deserialize(ref pseudoReader, options);
+            __result = StandardResolver.Instance.GetFormatterWithVerify<IViewData>().Deserialize(ref reader, options);
             return false;
         }
     }
