7764c9f342290d528f9da4966a87aeb5fee7a3a1
Zoey D. <zdavis8529@gmail.com>
2024-09-15 07:46:04 -0600

    client whatevs idk

--- 1.2.0/IViewDataFormatterPatches.cs
+++ 1.2.0/IViewDataFormatterPatches.cs
@@ -7,6 +7,7 @@ using MessagePack.Resolvers;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
+using UniverseLib.Utility;
 using Priority = HarmonyLib.Priority;
 
 namespace KitchenPlatePatch._120
@@ -25,15 +26,13 @@ namespace KitchenPlatePatch._120
         [HarmonyPriority(Priority.LowerThanNormal)]
         static bool Serialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref MessagePackWriter writer, IViewData value, MessagePackSerializerOptions options)
         {
-            if (!__runOriginal)
+            if (!__runOriginal || _typeToKeyAndJumpMap.IsNullOrDestroyed() || value == null)
                 return true;
             
             Dictionary<RuntimeTypeHandle, KeyValuePair<int, int>> typeToKeyAndJumpMap = (Dictionary<RuntimeTypeHandle, KeyValuePair<int, int>>)_typeToKeyAndJumpMap.GetValue(__instance);
             if (typeToKeyAndJumpMap.ContainsKey(value.GetType().TypeHandle))
-            {
-                writer.WriteInt32(605);
                 return true;
-            }
+
             writer.WriteInt32(784);
             StandardResolver.Instance.GetFormatterWithVerify<IViewData>().Serialize(ref writer, value, options);
             return false;
@@ -42,17 +41,15 @@ namespace KitchenPlatePatch._120
         // Deserialize
         [HarmonyPatch(nameof(IViewDataFormatter.Deserialize)), HarmonyPrefix]
         [HarmonyPriority(Priority.LowerThanNormal)]
-        static bool Deserialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref IViewData __result, ref MessagePackReader reader, MessagePackSerializerOptions options)
+        static bool Deserialize_Prefix(bool __runOriginal, IViewDataFormatter __instance, ref IViewData __result, MessagePackReader reader, MessagePackSerializerOptions options)
         {
-            if (!__runOriginal)
+            if (!__runOriginal || reader.TryReadNil() || reader.ReadArrayHeader() != 2 || _typeToKeyAndJumpMap.IsNullOrDestroyed())
                 return true;
 
             int modDetector = reader.ReadInt32();
-            if (modDetector == 605)
-            {
+            if (modDetector != 784)
                 return true;
-            }
-            if (modDetector != 784) return true;
+
             __result = StandardResolver.Instance.GetFormatterWithVerify<IViewData>().Deserialize(ref reader, options);
             return false;
         }
